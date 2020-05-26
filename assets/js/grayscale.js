7b9359f5937aff8705a5bebe515a5450b3e803e2
Daniel <thatrealdann@gmail.com>
2020-05-26 16:11:36 +0430

    Adjust gallery ratio

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -1,19 +1,12 @@
 (function($) {
   "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    
+    $(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').clearQueue().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000, 'easeInOutExpo');
+});
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
